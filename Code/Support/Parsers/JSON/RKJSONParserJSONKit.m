973436fc5df0b362f34244c43c45ebf703ed93e1
Kevin Cassidy <kcassidy@mindgrub.com>
2012-12-19 11:24:50 -0500

    Produce mutable objects from JSONKit parsing operations

--- Code/Support/Parsers/JSON/RKJSONParserJSONKit.m
+++ Code/Support/Parsers/JSON/RKJSONParserJSONKit.m
@@ -31,10 +31,10 @@
 // the internal caching capabilities from the JSONKit serializer
 @implementation RKJSONParserJSONKit
 
-- (NSDictionary *)objectFromString:(NSString *)string error:(NSError **)error
+- (NSMutableDictionary *)objectFromString:(NSString *)string error:(NSError **)error
 {
     RKLogTrace(@"string='%@'", string);
-    return [string objectFromJSONStringWithParseOptions:JKParseOptionStrict error:error];
+    return [string mutableObjectFromJSONStringWithParseOptions:JKParseOptionStrict error:error];
 }
 
 - (NSString *)stringFromObject:(id)object error:(NSError **)error
